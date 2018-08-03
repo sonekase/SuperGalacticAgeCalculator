68f9eeeeff0a4432aa6e75744f40d0154f1f723d
Anousone Kaseumsouk <anousonekaseumsouk@gmail.com>
2018-08-03 13:41:01 -0700

    Enter all methods and test.

--- spec/SuperGalacticAgeCalculator-spec.js
+++ spec/SuperGalacticAgeCalculator-spec.js
@@ -4,42 +4,108 @@ import { SuperGalacticAgeCalculator } from './../src/SuperGalacticAgeCalculator.
 describe('SuperGalacticAgeCalculator', function(){
 
   it('should calculate the age of someone into seconds', function() {
-    let calculator = new Calculator();
-    expect(calculator.ageInSeconds(21)).toEqual(662256000);
+  let calculator = new SuperGalacticAgeCalculator();
+  expect(calculator.ageInSeconds(21)).toEqual(662256000);
   });
 
   it('should calculate the seconds between two dates', function () {
-   let calculator = new Calculator();
-   let a = [2018, 0, 2];
-   let b = [2018, 0, 1];
-   expect(calculator.secondsBetweenTwoDates(a, b)).toEqual(86400);
- });
+  let calculator = new SuperGalacticAgeCalculator();
+  let dateOne = [2018, 0, 2];
+  let dateTwo = [2018, 0, 1];
+  expect(calculator.secondsBetweenDates(dateOne, dateTwo)).toEqual(86400);
+  });
 
- it('should calculate the age of someone into Earth years', function () {
-   expect(calculator.ageInYearsOnMercury(21)).toEqual(21);
- });
+  it('should calculate the age of someone into Earth years', function () {
+  let calculator = new SuperGalacticAgeCalculator();
+  expect(calculator.ageInYearsOnEarth(21)).toEqual(21);
+  });
 
- it('should calculate the age of someone into Mercury years', function () {
-   expect(calculator.ageInYearsOnMercury(21)).toEqual(87);
- });
+  it('should calculate the age of someone into Mercury years', function () {
+  let calculator = new SuperGalacticAgeCalculator();
+  expect(calculator.ageInYearsOnMercury(21)).toEqual(87);
+  });
 
- it('should calculate the age of someone into Venus years', function () {
+  it('should calculate the age of someone into Venus years', function () {
+  let calculator = new SuperGalacticAgeCalculator();
   expect(calculator.ageInYearsOnVenus(21)).toEqual(33);
-});
+  });
 
-it('should calculate the age of someone into Mars years', function () {
+  it('should calculate the age of someone into Mars years', function () {
+  let calculator = new SuperGalacticAgeCalculator();
   expect(calculator.ageInYearsOnMars(21)).toEqual(11);
-});
+  });
 
-it('should calculate the age of someone into Jupiter years', function () {
+  it('should calculate the age of someone into Jupiter years', function () {
+  let calculator = new SuperGalacticAgeCalculator();
   expect(calculator.ageInYearsOnJupiter(21)).toEqual(1);
-});
+  });
 
+  it('should return the max life of a male person on Mercury', function () {
+  let calculator = new SuperGalacticAgeCalculator();
+  expect(calculator.maxLifeOnPlanet("male", "Mercury")).toEqual(414);
+  });
 
+  it('should return the max life of a female person on Mercury', function () {
+  let calculator = new SuperGalacticAgeCalculator();
+  expect(calculator.maxLifeOnPlanet("female", "Mercury")).toEqual(435);
+  });
 
+  it('should return the max life of a male person on Venus', function () {
+  let calculator = new SuperGalacticAgeCalculator();
+  expect(calculator.maxLifeOnPlanet("male", "Venus")).toEqual(162);
+  });
 
+  it('should return the max life of a female person on Venus', function () {
+  let calculator = new SuperGalacticAgeCalculator();
+  expect(calculator.maxLifeOnPlanet("female", "Venus")).toEqual(170);
+  });
 
+  it('should return the max life of a male person on Mars', function () {
+  let calculator = new SuperGalacticAgeCalculator();
+  expect(calculator.maxLifeOnPlanet("male", "Mars")).toEqual(53);
+  });
 
+  it('should return the max life of a female person on Mars', function () {
+  let calculator = new SuperGalacticAgeCalculator();
+  expect(calculator.maxLifeOnPlanet("female", "Mars")).toEqual(55);
+  });
 
+  it('should return the max life of a male person on Jupiter', function () {
+  let calculator = new SuperGalacticAgeCalculator();
+  expect(calculator.maxLifeOnPlanet("male", "Jupiter")).toEqual(8);
+  });
+
+  it('should return the max life of a female person on Jupiter', function () {
+  let calculator = new SuperGalacticAgeCalculator();
+  expect(calculator.maxLifeOnPlanet("female", "Jupiter")).toEqual(8);
+  });
+
+  it('should return difference in currentAge compare to maxAge on Mercury', function () {
+  let currentAge = 420;
+  let maxAge = 414;
+  let calculator = new SuperGalacticAgeCalculator();
+  expect(calculator.pastMaxLifeOnPlanet("Mercury", currentAge, maxAge)).toEqual(6);
+  });
+
+  it('should return difference in currentAge compare to maxAge on Venus', function () {
+  let currentAge = 168;
+  let maxAge = 162;
+  let calculator = new SuperGalacticAgeCalculator();
+  expect(calculator.pastMaxLifeOnPlanet("Venus", currentAge, maxAge)).toEqual(6);
+  });
+
+  it('should return difference in currentAge compare to maxAge on Mars', function () {
+  let currentAge = 59;
+  let maxAge = 53;
+  let calculator = new SuperGalacticAgeCalculator();
+  expect(calculator.pastMaxLifeOnPlanet("Mars", currentAge, maxAge)).toEqual(6);
+  });
+
+  it('should return difference in currentAge compare to maxAge on Jupiter', function () {
+  let currentAge = 14;
+  let maxAge = 8;
+  let calculator = new SuperGalacticAgeCalculator();
+  expect(calculator.pastMaxLifeOnPlanet("Jupiter", currentAge, maxAge)).toEqual(6);
+  });
 
 });
--- src/SuperGalacticAgeCalculator.js
+++ src/SuperGalacticAgeCalculator.js
@@ -1,8 +1,8 @@
-var moment = require('moment');
+import moment from "moment"
 
 export class SuperGalacticAgeCalculator {
-constructor () {
-}
+  constructor () {
+  }
 
   ageInSeconds(age) {
     let secondsInYear = (age * 31536000);
@@ -11,16 +11,16 @@ constructor () {
 
 //Moment.js Parse, validate, manipulate, and display dates and times in JavaScript.
   secondsBetweenDates(date1, date2) {
-    let a = moment(date1);
-    let b = moment(date2);
-    let result = a.diff(b, 'seconds');
+    let dateOne = moment(date1);
+    let dateTwo = moment(date2);
+    let result = dateOne.diff(dateTwo, 'seconds');
     return result;
   }
 
   ageInYearsOnEarth(age) {
-  let ageOnEarth = Math.floor(age / 1);
-  return ageOnEarth;
-}
+    let ageOnEarth = Math.floor(age / 1);
+    return ageOnEarth;
+  }
 
   ageInYearsOnMercury(age) {
     let ageOnMercury = Math.floor(age / 0.24);
@@ -35,14 +35,43 @@ constructor () {
   ageInYearsOnMars(age) {
    let ageOnMars = Math.floor(age / 1.88);
    return ageOnMars;
- }
+  }
 
- ageInYearsOnJupiter(age) {
+  ageInYearsOnJupiter(age) {
    let ageOnJupiter = Math.floor(age / 11.86);
    return ageOnJupiter;
+  }
+
+  maxLifeOnPlanet(sex, planet) {
+   if(sex === "male" && planet === "Mercury") {
+     return 414;
+    } else if(sex === "female" && planet === "Mercury") {
+     return 435;
+    } else if(sex === "male" && planet === "Venus") {
+     return 162;
+    } else if(sex === "female" && planet === "Venus") {
+     return 170;
+    } else if(sex === "male" && planet === "Mars") {
+    return 53;
+    } else if(sex === "female" && planet === "Mars") {
+    return 55;
+    } else if(sex === "male" && planet === "Jupiter") {
+    return 8;
+    } else if(sex === "female" && planet === "Jupiter") {
+    return 8;
+   }
  }
 
-
-
+ pastMaxLifeOnPlanet(planet, currentAge, maxAge) {
+   if (planet === "Mercury" && currentAge > maxAge) {
+     return currentAge - maxAge;
+   } else if (planet === "Venus" && currentAge > maxAge) {
+     return currentAge - maxAge;
+   } else if (planet === "Mars" && currentAge > maxAge) {
+     return currentAge - maxAge;
+   } else if (planet === "Jupiter" && currentAge > maxAge) {
+     return currentAge - maxAge;
+   }
+ }
 
 }
